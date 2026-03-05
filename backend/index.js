91b79f3aab45492ae33d7601055b8719f3a3720b
winniem30 <tejaswinim446@gmail.com>
2026-03-05 22:21:57 +0530

    Add environment variable for MongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/pet-battle')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pet-battle')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
